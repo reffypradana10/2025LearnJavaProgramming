2d8ac69014f38bb8c5f0ce71632e56edd7845bb7
reffypradana10 <reffypradana10@gmail.com>
2025-01-01 22:49:29 +0700

    Tipe Data String

---
+++ src/TipeDataString.java
@@ -0,0 +1,11 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Refi Yusuf Pradana";
+        String lastname = "Programmer Hebat";
+
+        System.out.println(firstName);
+        System.out.println(lastname);
+
+        System.out.println(firstName + " Adalah : " + lastname);
+    }
+}
