b0d00d6e9056f43815836417716028b3076b9fa2
reffypradana10 <reffypradana10@gmail.com>
2025-01-01 22:33:20 +0700

    Tipe Data Number

---
+++ src/KonversiNumber.java
@@ -0,0 +1,16 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+
+        // Konversi Otomatis
+        byte iniByte = 30;
+        short iniShort = iniByte;
+        int intInt = iniShort;
+        System.out.println(intInt);
+
+        // Konversi Kebalikan
+        double iniDouble = 100.5;
+        int iniInt2 = (int) iniDouble;
+
+        System.out.println(iniInt2);
+    }
+}
---
+++ src/TIpeDataNumber.java
@@ -0,0 +1,22 @@
+public class TIpeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 1000000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000L;
+
+        float iniFloat = 10.12F;
+        double iniDouble = 10.12;
+
+        // Literal
+        int decimalInt = 34;
+        int hexaDecimal = 0xFFFFF;
+        int binaryDecimal = 0b101010;
+
+
+        // Kode Underscore
+        int amount = 1_000_000;
+
+    }
+}
