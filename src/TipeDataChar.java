b6c3e4d1491401c1cfb8db9000c56dfa51f331e7
reffypradana10 <reffypradana10@gmail.com>
2025-01-01 22:40:35 +0700

    Tipe Data Character

---
+++ src/TipeDataChar.java
@@ -0,0 +1,14 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char e = 'E';
+        char r = 'R';
+        char f = 'F';
+        char i = 'I';
+
+        System.out.print(r);
+        System.out.print(e);
+        System.out.print(f);
+        System.out.print(i);
+
+    }
+}
