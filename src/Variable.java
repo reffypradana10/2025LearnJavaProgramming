385a03d67dbbd83391d710e77e8ecdb21572a3f5
reffypradana10 <reffypradana10@gmail.com>
2025-01-01 23:00:50 +0700

    Variable

---
+++ src/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "refi yusuf pradana";
+
+        System.out.println(name);
+
+        int age = 22;
+        String address = "Blitar";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Mame Kudasi";
+        System.out.println(name);
+
+        var firstname = "Rephiii";
+        var lastname = "Pradana";
+
+        final String nama = "Refi Yusuf Pradana";
+        System.out.println(nama);
+    }
+}
