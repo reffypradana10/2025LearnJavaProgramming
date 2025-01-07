a88033e101779e822a8e65cfbd1805f4adaf4ab1
reffypradana10 <reffypradana10@gmail.com>
2025-01-07 16:42:10 +0700

    Array

---
+++ src/Array.java
@@ -0,0 +1,42 @@
+public class Array {
+    public static void main(String[] args) {
+
+
+        String[] stringarray;
+        stringarray = new String[3];
+
+        stringarray[0] = "Refi";
+        stringarray[1] = "Yusuf";
+        stringarray[2] = "Pradana";
+
+        System.out.println(stringarray[0]);
+        System.out.println(stringarray[1]);
+        System.out.println(stringarray[2]);
+
+        String[] stringarray2 = new String[3];
+
+
+        String[] namanama = {
+                "Refi", "Yusuf","Pradana"
+        };
+
+
+        int[] arrayInt = new int[]{
+                1,2,3,4,5,6
+        };
+
+        long[] arrayLong = new long[]{
+                10L,20L,30L
+        };
+
+        System.out.println(arrayLong.length);
+
+        String[][] member ={
+                {"refi", "yusuf"},
+                {"Eko"}
+        };
+
+        System.out.println(member[1][0]);
+
+    }
+}
