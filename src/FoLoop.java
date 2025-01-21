866e05e38665d5cafcab2a49274333d590959202
reffypradana10 <reffypradana10@gmail.com>
2025-01-21 21:02:45 +0700

    For Loop

---
+++ src/FoLoop.java
@@ -0,0 +1,10 @@
+public class FoLoop {
+    public static void main(String[] args) {
+
+
+        for(var counter = 1;counter <=10 ;counter++){
+            System.out.println("Hello World ke "+counter);
+
+        }
+    }
+}
