adc7c740d36ba7fbe89f664ac53aca4cbde37d38
reffypradana10 <reffypradana10@gmail.com>
2025-01-21 20:54:50 +0700

    Ternary Operator

---
+++ src/TernaaryOperator.java
@@ -0,0 +1,14 @@
+public class TernaaryOperator {
+    public static void main(String[] args) {
+        var nilai = 70;
+        String ucapan = nilai >= 75?"Selamat anda lulus":"Mohon coba lagi";
+
+//        if(nilai >= 75){
+//            ucapan = "Selamat anda lulus";
+//        }else{
+//            ucapan = "Mohon Coba lagi";
+//        };
+
+        System.out.println(ucapan);
+    }
+}
