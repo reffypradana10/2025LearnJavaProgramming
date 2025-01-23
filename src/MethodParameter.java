5dabe8fc0811db088d944eb19232fe4fa8e767eb
reffypradana10 <reffypradana10@gmail.com>
2025-01-23 23:24:12 +0700

    Method Parameter

---
+++ src/MethodParameter.java
@@ -0,0 +1,10 @@
+public class MethodParameter {
+    public static void main(String[] args) {
+        sayHello("rephi","pradana");
+        sayHello("alex","xandar");
+    }
+
+    static void sayHello(String firstname, String lastname){
+        System.out.println("Hello " + firstname + " " + lastname);
+    }
+}
