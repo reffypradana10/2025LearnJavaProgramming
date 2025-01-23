d99aac32dfd0c8fb9f1cbbd56dddc539e6465b1f
reffypradana10 <reffypradana10@gmail.com>
2025-01-23 23:39:49 +0700

    Method Variable Argument

---
+++ src/MethodVariableArgument.java
@@ -0,0 +1,24 @@
+public class MethodVariableArgument {
+    public static void main(String[] args) {
+
+        int[] values = {70,70,70,60,50,70,80};
+        sayCongraats("Rephi",values);
+
+        sayCongraats("Budi",80,70,100,100,100);
+    }
+
+    static void sayCongraats(String name, int... values){
+        var total = 0;
+        for(var value : values){
+            total+=value;
+        }
+
+        var finalValue = total / values.length;
+
+        if(finalValue >= 75){
+            System.out.println("Selamat "+name+", Anda Lulus");
+        }else{
+            System.out.println("Maaf "+name+", Anda Gagal");
+        }
+    }
+}
